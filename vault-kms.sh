8781c3a8dc5853fa44f25981a918a1ad3b9a7391
zakariaelmoumnaoui96 <zakaria.elmoumnaoui96@gmail.com>
2025-10-13 15:31:11 +0100

    fx: added wrapped token

--- vault-kms.sh
+++ vault-kms.sh
@@ -27,7 +27,8 @@ vault policy write app-db-read-b /tmp/app-db-read-b.hcl
 
 # 2.2 AppRole bound to that policy
 vault auth enable approle || true
-vault write auth/approle/role/app-approle-b \
+ROLE=app-approle-b
+vault write auth/approle/role/$ROLE \
   secret_id_num_uses=10 \
   secret_id_ttl=100m \
   token_ttl=130m \
@@ -35,14 +36,16 @@ vault write auth/approle/role/app-approle-b \
   token_policies="app-db-read-b"
 
 # 2.3 Get bootstrap values for Kubernetes Secret
-ROLE_ID=$(vault read  -field=role_id  auth/approle/role/app-approle-b/role-id)
-SECRET_ID=$(vault write -f -field=secret_id auth/approle/role/app-approle-b/secret-id)
+ROLE_ID=$(vault read  -field=role_id  auth/approle/role/$ROLE/role-id)
+SECRET_ID=$(vault write -f -field=secret_id auth/approle/role/$ROLE/secret-id)
+WRAP=$(vault write -wrap-ttl=100m -f -format=json auth/approle/role/$ROLE/secret-id | jq -r .wrap_info.token)
 echo "ROLE_ID=$ROLE_ID"
 echo "SECRET_ID=$SECRET_ID"
-
+echo "WRAP=$WRAP"
 # Result:
 ROLE_ID=603e9881-c5a0-6945-d175-f94fd040bb6e
 SECRET_ID=64237c41-03ce-fe9f-3014-08530d3e9aae
+WRAP=
 
 # Issue one dynamic cred
 vault read -format=json database/creds/app-db-role-b | tee /tmp/cred_b.json
